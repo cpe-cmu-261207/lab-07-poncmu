ffe3dd3a8439fcbd97ef7770ffc9dc08acf1df28
pondcmu <poncmu@gmail.com>
2021-02-09 03:13:55 +0700

    Pond

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,19 +1,62 @@
 import {useRouter} from "next/router";
 import {useEffect,useState} from 'react'
+import Link from 'next/link'
 import axios from 'axios'
 const baseURL = 'https://dummyapi.io/data/api'
+const APP_ID = '602142837fe6b0f78b2c17ea'
+
 const Post = () => {
     const router = useRouter()
     const {postId} = router.query
     const [post,setPost] = useState(null)
     const [comment,setComment] = useState([])
-
+    const fetch = async () => {
+        if(postId){
+            const response = await axios.get(`${baseURL}/post/${postId}`, 
+            { headers: 
+                { 'app-id': APP_ID } 
+            })
+            const comment = await axios.get(`${baseURL}/post/${postId}/comment`,
+             { headers: 
+                { 'app-id': APP_ID } 
+            })
+            setPost(response.data)
+            setComment(comment.data.data)
+            console.log(response.data)
+            console.log(comment.data.data)
+        }
+    }
+    useEffect(fetch,[postId])
     return (
         <>
-            <h1>display post data from api here</h1>
+            {post !== null ? (
+                <div>
+                    <p></p>
+                    <button><Link href='http://localhost:3000/'>Home</Link></button>
+        <button><Link href='\gallery'>Gallery</Link></button>
+        <button><Link href='\contact'>Contact</Link></button>
+        <button><Link href='\gpa'>Gpa Calculator</Link></button>
+        <button><Link href='\post'>Post</Link></button>
+                <hr/>
+                    <h1>Post : {post.text}</h1>
+                    <h2>tags : {[...post.tags].toString()}</h2>
+                    <img src = {post.image} width = {500} height = {500}></img>
+                    <p>{post.owner.firstName} {post.owner.lastName}</p>
+                    <p>Likes : {post.likes}</p>
+                    <h3>Comments</h3>
+                    <ul>
+                    {comment.map((comm) => {    
+                       return <li>{comm.owner.firstName} {comm.owner.lastName} : {comm.message}</li>
+                    })}
+                    </ul>
+                    <div >
+                        <button><Link href="/post">Back</Link></button>
+                    </div>
+                    
+                </div>
+            ):null}
         </>
     )
 }
 
 export  default  Post
-
--- pages/post/index.js
+++ pages/post/index.js
@@ -2,13 +2,47 @@ import {useEffect,useState} from 'react'
 import Link from 'next/link'
 import axios from 'axios'
 const baseURL = 'https://dummyapi.io/data/api'
+const APP_ID = '602142837fe6b0f78b2c17ea'
 
 const Posts = () => {
     const [posts,setPosts] = useState([])
-
+    const [loading, setLoading] = useState(false)
+    
+    useEffect(() => {
+        const fetch = async() => {
+            setLoading(true)
+            const response = await axios.get(
+                'https://dummyapi.io/data/api/post',
+                {
+                headers: {
+                    'app-id' : '602142837fe6b0f78b2c17ea'
+                }
+            })
+            console.log(response.data)
+            setPosts(response.data.data)
+            setLoading(false)
+        }
+        fetch()
+    }, [])
     return (
         <>
+        <p></p>
+        <button><Link href='http://localhost:3000/'>Home</Link></button>
+        <button><Link href='\gallery'>Gallery</Link></button>
+        <button><Link href='\contact'>Contact</Link></button>
+        <button><Link href='\gpa'>Gpa Calculator</Link></button>
+        <button><Link href='\post'>Post</Link></button>
+        <hr/>
             <h1>All Posts</h1>
+            {loading && <p>Loading...</p>}
+            {posts.map(post => 
+            <div>
+                <p>post : {post.text}</p>
+                <p><img src = {post.image} width = {500} height = {500}/></p>
+                <p>Likes : {post.likes}</p>
+                <p><button><Link href = {'post/'+ post.id}>Go to this page</Link></button></p>
+            </div>
+            )}
         </>
     )
 }
